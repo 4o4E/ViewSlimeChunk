d7d8096e9a09126421cb493a800231cd9cbb9e19
4o4E <869951226@qq.com>
2022-08-18 20:09:54 +0800

    更新版本至1.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,26 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.20"
+    kotlin("jvm") version "1.7.10"
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "top.e404"
-version = "1.0.2"
+version = "1.0.3"
 
 repositories {
     mavenLocal()
+    // spigot
     maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
     maven("https://oss.sonatype.org/content/groups/public/")
     mavenCentral()
 }
 
 dependencies {
+    // spigot
     compileOnly("org.spigotmc:spigot-api:1.13.2-R0.1-SNAPSHOT")
     // eplugin
-    implementation("top.e404:eplugin:1.0.2")
+    implementation("top.e404:eplugin:1.0.3")
     // Bstats
     implementation("org.bstats:bstats-bukkit:3.0.0")
 }
@@ -29,7 +31,8 @@ tasks.withType<KotlinCompile>() {
 
 tasks.shadowJar {
     archiveFileName.set("${project.name}-${project.version}.jar")
-    relocate("org.bstats", "top.e404.viewslimechunk.bstats")
+    relocate("org.bstats", "top.e404.viewslimechunk.relocate.bstats")
+    relocate("kotlin", "top.e404.viewslimechunk.relocate.kotlin")
     exclude("META-INF/*")
     doFirst {
         for (file in File("jar").listFiles() ?: arrayOf()) {
